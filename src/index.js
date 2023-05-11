102b0b7ff2bb5812b5b88c3714c1fbb725f57b6e
PoliukhovychAnna <116736506+PoliukhovychAnna@users.noreply.github.com>
2023-05-12 00:48:02 +0300

    create cool function

--- src/index.js
+++ src/index.js
@@ -1,23 +1,48 @@
 import axios from 'axios';
 
+
+const listOfBooks = document.querySelector('.js-container');
 const TOP_BOOKS = 'https://books-backend.p.goit.global/books/top-books';
 
-getTopBooks(TOP_BOOKS).then().catch();
+getTopBooks(TOP_BOOKS)
+  .then(resp => listOfBooks.insertAdjacentHTML('afterbegin', resp))
+  .catch();
 
 async function getTopBooks() {
     const response = await axios.get(TOP_BOOKS);
-    console.log(response);
-    const { category,title, rank, book_image, author, _id } = response.data.books
-    console.log(title);
-    // const topBooks = response
-    //   .sort(book => book.rank)
-    //   .map(({ title, rank, book_image, author, _id }) => {
-    //     const markup = `<li class="book-item">
-    //                 <img src="${book_image}" alt="${title}" width="180" height="256" />
-    //                 <h3 class="book-name">IT STARTS WITH US</h3>
-    //                 <p class="author-name">${author}</p>
-    //             </li>`.join();
-    //   });
-
-  
+    const data = response.data
+    const topBooks = data.map(({ list_name, books }) => {
+        return `<div class = "category-container"><h2 class="home-book-category">${list_name}</h2>
+        <ul class="books-list">
+                <li class="book-item">
+                <div class = "overlay"></div>
+                    <img class = "book-img" src="${books[0].book_image}" alt="Poster of ${books[0].title}" width="180" height="256" />
+                    <h3 class="book-name">${books[0].title}</h3>
+                    <p class="author-name">${books[0].author}</p>
+                </li>
+                <li class="book-item">
+                    <img class = "book-img" src="${books[1].book_image}" alt="Poster of ${books[1].title}" width="180" height="256" />
+                    <h3 class="book-name">${books[1].title}</h3>
+                    <p class="author-name">${books[1].author}</p>
+                </li>
+                <li class="book-item">
+                    <img class = "book-img" src="${books[2].book_image}" alt="Poster of ${books[2].title}" width="180" height="256" />
+                    <h3 class="book-name">${books[2].title}</h3>
+                    <p class="author-name">${books[2].author}</p>
+                </li>
+                <li class="book-item">
+                    <img class = "book-img" src="${books[3].book_image}" alt="Poster of ${books[3].title}" width="180" height="256" />
+                    <h3 class="book-name">${books[3].title}</h3>
+                    <p class="author-name">${books[3].author}</p>
+                </li>
+                <li class="book-item">
+                    <img class = "book-img" src="${books[4].book_image}" alt="Poster of ${books[4].title}" width="180" height="256" />
+                    <h3 class="book-name">${books[4].title}</h3>
+                    <p class="author-name">${books[4].author}</p>
+                </li>
+        </ul> <button class="btn-see-more" type="button">SEE MORE</button></div>`;
+        
+    }).join('');
+    console.log(topBooks);
+    return topBooks
 }
